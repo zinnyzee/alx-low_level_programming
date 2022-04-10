0b48d2e013d16139c08939ec1d998321e4b10c08
nduka <ndukaabigail1@gmail.com>
2022-04-10 14:23:52 -0700

    llp quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,20 @@
 /*
- *   File: 101-quote.c
- *    Auth: Zinnyzee
+ *  * File: 101-quote.c
+ *   * Auth: zinnyzee
  */
 
 #include <unistd.h>
 
 /**
- *  main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                 followed by a new line, to standard error.
- *
- *   Return: Always 1.
+ *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *   *                followed by a new line, to standard error.
+ *    *
+ *     * Return: Always 1.
  */
 int main(void)
 {
 	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",		      59);
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 
-	return (1);
-}
+	return (1);								}
