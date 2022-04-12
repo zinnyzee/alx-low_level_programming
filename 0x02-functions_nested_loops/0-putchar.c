bc860d11119bbc0700520d8c067d43bebd276b44
nduka <ndukaabigail1@gmail.com>
2022-04-12 14:50:23 -0700

    ' git add . git commit -m git add . ./0-putchar.c git commit -m "putchar" A Q

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ *  * File: 0-putchar.c
+ *   * Auth:zinnyzee
+ *    *
+ */
+
+#include "0-putchar.h"
+
+/**
+ *  * main - Prints "_putchar" followed by a new line.
+ *   *
+ *    * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);						}
