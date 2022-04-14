f9c1ffaf801cda2b1d6a3576790454c75c8893d0
nduka <ndukaabigail1@gmail.com>
2022-04-14 09:22:47 -0700

    main.c.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,11 @@
 /**
  *  * main - main
  *   * Return: 0
- */
-
+ */								
 int main(void)
 {
 	int i;
-	i = 0;
-
-	positive_or_negative(i);
-
-	return (0);
-
+	i = 0;							
+	positive_or_negative(i);				
+	return (0);						
 }
