d9a85737c8eef0bdd05d945735d5dff34e1bcdec
nduka <ndukaabigail1@gmail.com>
2022-04-14 20:06:32 -0700

    leap_year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,20 +11,16 @@
 * Return: void
 */
 
-
-void print_remaining_days(int month, int day, int year)
+void pint_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0 && year % 100 == 0) || (year % 4 == 0))
+	if ((year % 400 == 0 && year % 100 == 0) || (year % 4 ==0))
 	{
 		if (month >= 3 && day >= 60)
 		{
-
 			day++;
 		}
-
-
 		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		printf("Reamaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -32,12 +28,10 @@ void print_remaining_days(int month, int day, int year)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
-	
 		else
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-
 }
