736de9ea1c9a2a72f470f2dbbaef1cca8f08f28b
nduka <ndukaabigail1@gmail.com>
2022-04-23 16:24:05 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  *   *@dest: A pointer to a character that will be changed
  *    *@src: A pointer to a character that will also be changed
  *     *Return: dest
- *      */
+ */
 
 char *_strcat(char *dest, char *src)
 {
