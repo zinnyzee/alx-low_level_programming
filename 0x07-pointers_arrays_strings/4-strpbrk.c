c9d8d25eec896eb11aab1f363151538e1eea5d28
nduka <ndukaabigail1@gmail.com>
2022-04-27 12:32:34 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
-*  * _strpbrk - prints buffer in hexa
-*   * @s: buffer
-*    * @accept: buffer2
-*     *
-*      * Return: Nothing.
+* _strpbrk - prints buffer in hexa
+* @s: buffer
+* @accept: buffer2
+*
+* Return: Nothing.
 */
 char *_strpbrk(char *s, char *accept)
 {
-		int i, j;
+	int i, j;
 
-			for (i = 0; *(s + i); i++)
-					{
-								for (j = 0; *(accept + j); j++)
-											{
-															if (*(s + i) == *(accept + j))
-																			{
-																								return (s + i);
-																											}
-																	}
+	for (i = 0; *(s + i); i++)				{
+									for (j = 0; *(accept + j); j++)
+		{
+										if (*(s + i) == *(accept + j))
+			{
+				return (s + i);
+										}
 									}
-				return (0);
+									}
+		return (0);
 }
