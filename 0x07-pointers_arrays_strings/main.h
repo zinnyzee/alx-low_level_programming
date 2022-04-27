bb6ee7c7e5f4a4ee60127ea09a13784f1d03053f
nduka <ndukaabigail1@gmail.com>
2022-04-26 18:48:59 -0700

    memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,16 +2,16 @@
 #define MAIN_H
 
 /**
- *  * File: main.h
- *   * Auth: zinnyzee
- *    * Desc: Header file containing prototypes for all functions
+* File: main.h
+* Auth: zinnyzee
+* Desc: Header file containing prototypes for all functions
  *     *       used in the 0x07-pointers_arrays_strings directory.
  */
 
 #include <stddef.h>
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
