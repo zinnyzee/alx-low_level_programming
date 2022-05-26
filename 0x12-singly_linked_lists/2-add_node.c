96d2cc6781b363e88b66a279b9e99a7b30347ee7
nduka <ndukaabigail1@gmail.com>
2022-05-25 20:04:24 -0700

    2 main

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,19 +16,16 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	 (new == NULL)
 		return (NULL);
-
 		dup = strdup(str);
 		if (dup == NULL)
 		{
 			free(new);
 			return (NULL);
 		}
-
 	for (len = 0; str[len];)
 		len++;
-
 		new->str = dup;
 		new->len = len;
 		new->next = *head;
